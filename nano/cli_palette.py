e21e77ceeaf82d7690bf6cb4af6ba9c8082a1ad8
Cychih <michael66230@gmail.com>
2024-03-02 03:44:32 +0800

    Update cli_palette, accept color codes and print colored banners

--- nano/cli_palette.py
+++ nano/cli_palette.py
@@ -1,21 +1,33 @@
+import sys
+
 from . import lib_paint
 
 from .lib_paint import paint
 
 
 def main():
-    print('Format: ESC[30;48;5;{}m')
-    for color in range(0, 256):
-        print(paint(fg=0, bg=color)(' ' + str(color).rjust(3)), end='')
+    prog = sys.argv[0]
+    argv = sys.argv[1:]
 
-        if color < 16 and (color + 1) % 8 == 0:
-            print()
+    if not argv:
+        print('Format: ESC[30;48;5;{}m')
+        for color in range(0, 256):
+            print(paint(fg=0, bg=color)(' ' + str(color).rjust(3)), end='')
 
-        if color >= 16 and (color - 16 + 1) % 36 == 0:
-            print()
+            if color < 16 and (color + 1) % 8 == 0:
+                print()
 
-        if color in (15, 231):
-            print()
+            if color >= 16 and (color - 16 + 1) % 36 == 0:
+                print()
 
+            if color in (15, 231):
+                print()
 
-    print()
+        print()
+
+    else:
+        for arg in argv:
+            print(
+                    paint(fg=int(arg), bg=int(arg))(arg.rjust(3)) +
+                    paint(bg=int(arg))(' ' * 100)
+                    )
