e5c341f4dd2d77997fc56afbcc8af841ea54c75e
Cychih <michael66230@gmail.com>
2024-08-01 00:44:39 +0800

    Implement gradient_rgb(), using hsv as intermetiate color space

--- gradient.py
+++ gradient.py
@@ -6,21 +6,28 @@ import random
 def main():
     # a, b = 77, 147
     a, b = 70, 191
+    a, b = 214, 39
     # a, b = random.sample(range(16, 232), 2)
     # print(smol.color, super(smol.color))
     # print(smol.color256, super(smol.color256))
     # print(smol.rgb, super(smol.rgb))
-    # assert False
     # A = smol.color(a)
     # B = smol.color(b)
-    # print(smol.paint(smol.rgb('#FF5F00'))('test'))
-    # for color in smol.gradient(A, B):
+    A = smol.color('#FF0000')
+    B = smol.color('#00FF00')
+    # A = smol.color('#FFAF00')
+    # B = smol.color('#00AFFF')
+    # A = smol.color('#FF1100')
+    # B = smol.color('#FF0011')
+    for color in smol.gradient(A, B, 20):
+        print(smol.paint(color)(color))
+
+    # A = smol.color(241)
+    # B = smol.color(250)
+    # for color in smol.gradient(A, B, N=4):
     #     print(smol.paint(color)(color))
 
-    A = smol.color(241)
-    B = smol.color(250)
-    for color in smol.gradient(A, B, N=4):
-        print(smol.paint(color)(color))
+    # print(smol.paint(smol.rgb('#FF5F00'))('test'))
 
 
 if __name__ == '__main__':
--- smol/paints.py
+++ smol/paints.py
@@ -12,10 +12,6 @@ def is_uint8(i):
     return isinstance(i, int) and 0 <= i < 256
 
 
-def lerp(a, b, t):
-    return ((1 - t) * a) + (t * b)
-
-
 class Color(abc.ABC):
     @abc.abstractmethod
     def __init__(self, *args, **kwargs):
@@ -89,10 +85,13 @@ class color256(Color):
 
 class rgb(Color):
     def __init__(self, *args):
+        if len(args) == 1 and isinstance(args[0], (tuple, list)):
+            args = args[0]
+
         if len(args) == 1 and isinstance(args[0], self.__class__):
             other = args[0]
             (self.r, self.g, self.b) = (other.r, other.g, other.b)
-        elif len(args) == 1 and re.match(r'^#[0-9a-f]{6}$', args[0].lower()):
+        elif len(args) == 1 and isinstance(args[0], str) and re.match(r'^#[0-9a-f]{6}$', args[0].lower()):
             rgb_str = args[0][1:]
             self.r = int(rgb_str[0:2], 16)
             self.g = int(rgb_str[2:4], 16)
@@ -117,6 +116,9 @@ class rgb(Color):
 
     def __int__(self):
         return (self.r << 16) | (self.g << 8) | (self.b)
+
+    def __str__(self):
+        return '#{:0>2X}{:0>2X}{:0>2X}'.format(self.r, self.g, self.b)
 
 
 class paint:
@@ -171,9 +173,10 @@ decolor_regex = re.compile('\033' + r'\[[\d;]*m')
 def decolor(s):
     return decolor_regex.sub('', s)
 
+
 def gradient(A, B, N=None):
     if not isinstance(A, Color) or not isinstance(B, Color):
-        raise TypeError('Cannot calculate gradient() on non-Color objects')
+        raise TypeError('Can only calculate gradient() on Color objects')
 
     if N is not None and not isinstance(N, int):
         raise TypeError('N must be a integer')
@@ -184,86 +187,133 @@ def gradient(A, B, N=None):
     if N == 2:
         return (A, B)
 
-    if not isinstance(A, color256) or not isinstance(B, color256):
-        return (A, B)
+    if isinstance(A, color256) and isinstance(B, color256):
+        return gradient_color256(A, B, N=N)
 
-    if A.code in range(232, 256) and B.code in range(232, 256):
-        return gradient_color256_gray(A, B, N)
-
-    if A.code in range(16, 232) and B.code in range(16, 232):
-        def color_to_rgb6(p):
-            c = int(p) - 16
-            r = c // 36
-            g = (c % 36) // 6
-            b = c % 6
-            return (r, g, b)
-
-        def rgb6_to_color(rgb):
-            return color256(rgb[0] * 36 + rgb[1] * 6 + rgb[2] + 16)
-
-        def rgb_add(a, b):
-            return (a[0] + b[0], a[1] + b[1], a[2] + b[2])
-
-        rgb_a = color_to_rgb6(A)
-        rgb_b = color_to_rgb6(B)
-
-        if N == 3:
-            return (A, rgb6_to_color(tuple(((rgb_a[i] + rgb_b[i]) // 2) for i in (0, 1, 2))), B)
-
-        delta = tuple(rgb_b[i] - rgb_a[i] for i in (0, 1, 2))
-        # print('delta', delta)
-
-        def sgn(i):
-            return (i > 0) - (i < 0)
-
-        steps = []
-        for n in range(max(map(abs, delta))):
-            steps.append(tuple(sgn(delta[i]) for i in (0, 1, 2)))
-            delta = tuple(delta[i] - sgn(delta[i]) for i in (0, 1, 2))
-
-        acc = [rgb_a]
-        for step in steps:
-            acc.append(rgb_add(acc[-1], step))
-
-        if N is None:
-            ret = acc
-        else:
-            dup, r = divmod(N, len(acc))
-            ret = []
-            for i in range(len(acc)):
-                for d in range(dup + (i < r)):
-                    ret.append(acc[i])
-
-        return tuple(rgb6_to_color(i) for i in ret)
+    if isinstance(A, rgb) and isinstance(B, rgb):
+        return gradient_rgb(A, B, N=N)
 
     return (A, B)
 
 
-def gradient_color256_gray(A, B, N):
-        a, b = A.code, B.code
-        reverse = (b < a)
-        if reverse:
-            a, b = b, a
-        n = (b + 1 - a)
+def gradient_color256(A, B, N=None):
+    if A.code in range(232, 256) and B.code in range(232, 256):
+        return gradient_color256_gray(A, B, N)
 
-        if N is None or N == n:
-            ret = tuple(color256(c) for c in range(a, b + 1))
+    if A.code in range(16, 232) and B.code in range(16, 232):
+        return gradient_color256_rgb(A, B, N)
 
-        if N < n:
-            # Discrete averaging skipped colors to fit N
-            skips = b - a + 1 - N
-            step = skips // (N - 1) + 1
-            ret = (color256(a),) + tuple(color256(a + (t * step)) for t in range(1, N-1)) + (color256(b),)
+    return (A, B)
 
-        if N > n:
-            # Duplicate colors to match N
-            ret = []
-            dup, r = divmod(N, n)
-            for i in range(a, b+1):
-                for d in range(dup + (i < r + a)):
-                    ret.append(color256(i))
 
-        if reverse:
-            ret = ret[::-1]
+def gradient_color256_gray(A, B, N=None):
+    a, b = A.code, B.code
+    reverse = (b < a)
+    if reverse:
+        a, b = b, a
+    n = (b + 1 - a)
 
-        return ret
+    if N is None or N == n:
+        ret = tuple(color256(c) for c in range(a, b + 1))
+
+    if N < n:
+        # Discrete averaging skipped colors to fit N
+        skips = b - a + 1 - N
+        step = skips // (N - 1) + 1
+        ret = (color256(a),) + tuple(color256(a + (t * step)) for t in range(1, N-1)) + (color256(b),)
+
+    if N > n:
+        # Duplicate colors to match N
+        ret = []
+        dup, r = divmod(N, n)
+        for i in range(a, b+1):
+            for d in range(dup + (i < r + a)):
+                ret.append(color256(i))
+
+    if reverse:
+        ret = ret[::-1]
+
+    return ret
+
+
+def gradient_color256_rgb(A, B, N=None):
+    def color_to_rgb6(p):
+        c = int(p) - 16
+        r = c // 36
+        g = (c % 36) // 6
+        b = c % 6
+        return (r, g, b)
+
+    def rgb6_to_color(rgb):
+        return color256(rgb[0] * 36 + rgb[1] * 6 + rgb[2] + 16)
+
+    def rgb_add(a, b):
+        return (a[0] + b[0], a[1] + b[1], a[2] + b[2])
+
+    rgb_a = color_to_rgb6(A)
+    rgb_b = color_to_rgb6(B)
+
+    if N == 3:
+        return (A, rgb6_to_color(tuple(((rgb_a[i] + rgb_b[i]) // 2) for i in (0, 1, 2))), B)
+
+    delta = tuple(rgb_b[i] - rgb_a[i] for i in (0, 1, 2))
+
+    def sgn(i):
+        return (i > 0) - (i < 0)
+
+    steps = []
+    for n in range(max(map(abs, delta))):
+        steps.append(tuple(sgn(delta[i]) for i in (0, 1, 2)))
+        delta = tuple(delta[i] - sgn(delta[i]) for i in (0, 1, 2))
+
+    acc = [rgb_a]
+    for step in steps:
+        acc.append(rgb_add(acc[-1], step))
+
+    if N is None:
+        ret = acc
+    else:
+        dup, r = divmod(N, len(acc))
+        ret = []
+        for i in range(len(acc)):
+            for d in range(dup + (i < r)):
+                ret.append(acc[i])
+
+    return tuple(rgb6_to_color(i) for i in ret)
+
+
+def lerp(a, b, t):
+    if t == 0:
+        return a
+    if t == 1:
+        return b
+    if isinstance(a, (tuple, list)):
+        return type(a)(a[i] + t * (b[i] - a[i]) for i in range(len(a)))
+
+    return a + t * (b - a)
+
+
+def gradient_rgb(A, B, N):
+    # Calculate gradient on RGB
+    # a = (A.r, A.g, A.b)
+    # b = (B.r, B.g, B.b)
+    #
+    # ret = [A]
+    # for t in (i / (N - 1) for i in range(1, N - 1)):
+    #     ret.append(rgb(tuple(map(int, lerp(a, b, t)))))
+    # ret.append(B)
+    # return tuple(ret)
+
+    import colorsys
+    # Calculate gradient on HSV
+    a = colorsys.rgb_to_hsv(A.r / 255, A.g / 255, A.b / 255)
+    b = colorsys.rgb_to_hsv(B.r / 255, B.g / 255, B.b / 255)
+
+    ret = [A]
+    for t in (i / (N - 1) for i in range(1, N - 1)):
+        c = tuple(lerp(a[j], b[j], t) for j in (0, 1, 2))
+        ret.append(rgb(tuple(int(255 * ca) for ca in colorsys.hsv_to_rgb(*c))))
+
+    ret.append(B)
+
+    return tuple(ret)
