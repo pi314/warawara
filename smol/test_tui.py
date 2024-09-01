69ecd26e07501a455899767dfa3ef51726340f31
Cychih <michael66230@gmail.com>
2024-09-02 00:56:59 +0800

    Merge lib_typesetting into lib_tui

--- smol/test_tui.py
+++ smol/test_tui.py
@@ -11,6 +11,150 @@ from .test_utils import *
 from smol import *
 
 
+class TestTypesettingUtils(TestCase):
+    def test_strwidth(self):
+        self.eq(strwidth('test'), 4)
+        self.eq(strwidth(orange('test')), 4)
+        self.eq(strwidth('哇嗚'), 4)
+
+    def test_ljust_str(self):
+        self.eq(ljust('test', 10), 'test      ')
+        self.eq(rjust('test', 10), '      test')
+
+        padding = ' ' * 6
+        self.eq(ljust(orange('test'), 10), orange('test') + padding)
+        self.eq(rjust(orange('test'), 10), padding + orange('test'))
+
+        padding = '#' * 6
+        self.eq(ljust(orange('test'), 10, '#'), orange('test') + padding)
+        self.eq(rjust(orange('test'), 10, '#'), padding + orange('test'))
+
+    def test_just_rect(self):
+        data = [
+                ('column1', 'col2'),
+                ('word1', 'word2'),
+                ('word3', 'word4 long words'),
+                ]
+
+        self.eq(ljust(data), [
+            ('column1', 'col2            '),
+            ('word1  ', 'word2           '),
+            ('word3  ', 'word4 long words'),
+            ])
+
+        self.eq(rjust(data), [
+            ('column1', '            col2'),
+            ('  word1', '           word2'),
+            ('  word3', 'word4 long words'),
+            ])
+
+    def test_just_with_fillchar(self):
+        data = [
+                ('column1', 'col2'),
+                ('word1', 'word2'),
+                ('word3', 'word4 long words'),
+                ]
+
+        self.eq(ljust(data, fillchar='#'), [
+            ('column1', 'col2############'),
+            ('word1##', 'word2###########'),
+            ('word3##', 'word4 long words'),
+            ])
+
+    def test_just_with_fillchar_func(self):
+        data = [
+                ('up left',   'up',   'up right'),
+                ('left',      '',     'right'),
+                ('down left', 'down', 'down r'),
+                ]
+
+        def fillchar(row, col, text):
+            if row + col == 2:
+                return '%'
+            if text == 'right':
+                return '$'
+            return '#' if (row % 2) else '@'
+
+        self.eq(ljust(data, fillchar=fillchar, width=10), [
+            ('up left@@@', 'up@@@@@@@@', 'up right%%'),
+            ('left######', '%%%%%%%%%%', 'right$$$$$'),
+            ('down left%', 'down@@@@@@', 'down r@@@@'),
+            ])
+
+        self.eq(rjust(data, fillchar=fillchar, width=10), [
+            ('@@@up left', '@@@@@@@@up', '%%up right'),
+            ('######left', '%%%%%%%%%%', '$$$$$right'),
+            ('%down left', '@@@@@@down', '@@@@down r'),
+            ])
+
+    def test_just_with_width(self):
+        data = [
+                ('column1', 'col2'),
+                ('word1', 'word2'),
+                ('word3', 'word4 long words'),
+                ]
+
+        self.eq(ljust(data, width=20), [
+            ('column1             ', 'col2                '),
+            ('word1               ', 'word2               '),
+            ('word3               ', 'word4 long words    '),
+            ])
+
+        self.eq(ljust(data, width=(10, 20)), [
+            ('column1   ', 'col2                '),
+            ('word1     ', 'word2               '),
+            ('word3     ', 'word4 long words    '),
+            ])
+
+    def test_just_with_generator(self):
+        data = [
+                ('column1', 'col2'),
+                ('word1', 'word2'),
+                ('word3', 'word4 long words'),
+                ]
+
+        ret = ljust((vector for vector in data), width=(10, 20))
+        self.is_false(isinstance(ret, (tuple, list)))
+
+        self.eq(list(ret), [
+            ('column1   ', 'col2                '),
+            ('word1     ', 'word2               '),
+            ('word3     ', 'word4 long words    '),
+            ])
+
+    def test_just_rect_lack_columns(self):
+        self.eq(
+                ljust([
+                    ('column1', 'col2'),
+                    ('word1',),
+                    tuple(),
+                    ('', 'multiple words'),
+                    tuple(),
+                    ]),
+                [
+                    ('column1', 'col2          '),
+                    ('word1  ', '              '),
+                    ('       ', '              '),
+                    ('       ', 'multiple words'),
+                    ('       ', '              '),
+                    ])
+
+    def test_just_rect_more_columns(self):
+        self.eq(
+                ljust([
+                    ('column1', 'col2'),
+                    tuple(),
+                    ('word1', 'word2', 'word4'),
+                    ('word3', 'multiple words'),
+                    ]),
+                [
+                    ('column1', 'col2          ', '     '),
+                    ('       ', '              ', '     '),
+                    ('word1  ', 'word2         ', 'word4'),
+                    ('word3  ', 'multiple words', '     '),
+                    ])
+
+
 def queue_to_list(Q):
     ret = []
     while not Q.empty():
