cb3899e974bc559fe0ceb7c831795703d759e998
Cychih <michael66230@gmail.com>
2024-11-18 01:34:24 +0800

    Add boilerplate code to rere(); Tests to be added

--- warawara/lib_regex.py
+++ warawara/lib_regex.py
@@ -9,11 +9,27 @@ class rere:
         self.text = text
         self.cache = None
 
-    def match(self, pattern):
-        self.cache = re.match(pattern, self.text)
+    def search(self, pattern, flags=0):
+        self.cache = re.search(pattern, self.text, flags=flags)
         return self.cache
 
+    def match(self, pattern, flags=0):
+        self.cache = re.match(pattern, self.text, flags=flags)
+        return self.cache
+
+    def fullmatch(self, pattern, flags=0):
+        self.cache = re.fullmatch(pattern, self.text, flags=flags)
+        return self.cache
+
+    def split(self, pattern, maxsplit=0, flags=0):
+        return re.split(pattern, self.text, maxsplit=maxsplit, flags=flags)
+
+    def findall(self, pattern, flags=0):
+        return re.findall(pattern, self.text, flags=flags)
+
     def __getattr__(self, attr):
+        if hasattr(re, attr):
+            result = getattr(re, attr)
         return getattr(self.cache, attr)
 
     def sub(self, pattern, repl):
