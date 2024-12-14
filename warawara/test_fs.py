b4e6c02f76d896653f97b34390de62870289d75d
Cychih <michael66230@gmail.com>
2024-12-15 01:54:18 +0800

    [test/fs] Add test case for open()

--- warawara/test_fs.py
+++ warawara/test_fs.py
@@ -1,9 +1,34 @@
+import unittest.mock
+
 from .test_utils import *
 
 import warawara as wrwr
 
 
 class TestFs(TestCase):
+    def test_open(self):
+        mock_open = unittest.mock.mock_open()
+        self.patch('builtins.open', mock_open)
+
+        with wrwr.open('warawara.txt', 'w') as f:
+            f.write('normal write\n')
+            f.writeline()
+            f.writeline('line1')
+            f.writelines(['line2', 'line3', 'line4'])
+
+        mock_open.assert_called_once_with(
+                'warawara.txt', 'w',
+                encoding='utf-8', errors='backslashreplace')
+        handle = mock_open()
+        handle.write.assert_has_calls([
+            unittest.mock.call('normal write\n'),
+            unittest.mock.call('\n'),
+            unittest.mock.call('line1\n'),
+            unittest.mock.call('line2\n'),
+            unittest.mock.call('line3\n'),
+            unittest.mock.call('line4\n'),
+            ])
+
     def test_fsorted(self):
         self.eq(wrwr.fsorted([
             'apple1',
