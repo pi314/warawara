8fdf8a63302a33ef4e85eeaa3333fb71bf5dc9fa
Cychih <michael66230@gmail.com>
2026-01-20 00:33:24 +0800

    [tui] Centalize onkey.setter() logic to onkey object itself

--- iroiro/lib_tui.py
+++ iroiro/lib_tui.py
@@ -1079,8 +1079,7 @@ class Menu:
             self.format = '{cursor} {item.text}'
 
         self._onkey = MenuKeyHandler(self)
-        if onkey:
-            self._onkey += onkey
+        self.onkey = onkey
 
         self.cursor_symbol = cursor
         self._cursor = MenuCursor(self, wrap=wrap)
@@ -1158,8 +1157,7 @@ class Menu:
 
     @onkey.setter
     def onkey(self, value):
-        self._onkey = MenuKeyHandler(self)
-        self._onkey += value
+        self._onkey.set_to(value)
 
     @property
     def first(self):
@@ -1507,10 +1505,7 @@ class MenuItem(MenuItemRef):
 
     @onkey.setter
     def onkey(self, value):
-        if self._onkey is value:
-            return
-        self._onkey.clear()
-        self._onkey += value
+        self._onkey.set_to(value)
 
     @property
     def index(self):
@@ -1729,6 +1724,13 @@ class MenuKeyHandler:
 
     def __call__(self, *args):
         return self.bind(*args)
+
+    def set_to(self, value):
+        if value is self:
+            return
+        self.clear()
+        if value is not None:
+            self += value
 
     def bind(self, *args):
         if len(args) == 1:
