0e221a2efe7c07bf8154bd372d74cf99999bb986
Cychih <michael66230@gmail.com>
2026-05-28 02:06:46 +0800

    [bin] Add decolor

---
+++ iroiro/bin_decolor.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import argparse
+import shutil
+
+from . import lib_colors as colors
+
+
+def main():
+    prog = sys.argv[0]
+    argv = sys.argv[1:]
+
+    term_size = shutil.get_terminal_size()
+
+    parser = argparse.ArgumentParser(description='nowrap', prog='nowrap')
+    parser.add_argument('-w', '--width', type=int, help='Width limit')
+
+    args = parser.parse_args(argv)
+
+    try:
+        for line in sys.stdin:
+            print(colors.decolor(line.rstrip('\n')))
+    except BrokenPipeError:
+        # https://docs.python.org/3/library/signal.html#note-on-sigpipe
+        devnull = os.open(os.devnull, os.O_WRONLY)
+        os.dup2(devnull, sys.stdout.fileno())
